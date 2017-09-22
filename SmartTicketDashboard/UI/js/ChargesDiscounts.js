d43ee7dba95b4e0fcd20ec9cafa75231d7f033da
gundapurna <venkatapurnachandrarao71@gmail.com>
2017-09-22 18:39:20 +0530

    changes

---
+++ SmartTicketDashboard/UI/js/ChargesDiscounts.js
@@ -0,0 +1,23 @@
+﻿var app = angular.module('myApp', ['ngStorage', 'ui.bootstrap', 'angularFileUpload']);
+
+var ctrl =app.controller('myCtrl', function ($scope, $http, $localStorage, $uibModal, $upload) {
+    if ($localStorage.uname == null) {
+        window.location.href = "login.html";
+    }
+    $scope.uname = $localStorage.uname;
+    $scope.userdetails = $localStorage.userdetails;
+    $scope.Roleid = $scope.userdetails[0].roleid;
+
+    $scope.dashboardDS = $localStorage.dashboardDS;
+
+    $scope.GetChargesDiscounts = function () {
+
+        //$scope.selectedChargeslist = parseLocation(window.location.search)['DId'];
+
+        $http.get('/api/GetAllChargesDiscounts').then(function (res, data) {
+            $scope.Chargeslist = res.data[0];
+
+        });
+    }
+
+});
