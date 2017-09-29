f6c980d67eb5fac227e36761cc2ea2347ac0d38d
srujansimha <msrujansimha@gmail.com>
2017-09-29 18:14:03 +0530

    All stops in seperate page

---
+++ SmartTicketDashboard/Controllers/TaxiSrcDestController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SmartTicketDashboard.Controllers
+{
+    public class TaxiSrcDestController : ApiController
+    {
+        [HttpGet]
+        [Route("api/GetTaxiStopsList")]
+        public DataTable GetTaxiStopsList()
+        {
+            DataTable Tbl = new DataTable();
+
+            //connect to database
+            SqlConnection conn = new SqlConnection();
+            //connetionString="Data Source=ServerName;Initial Catalog=DatabaseName;User ID=UserName;Password=Password"
+            conn.ConnectionString = System.Configuration.ConfigurationManager.ConnectionStrings["btposdb"].ToString();
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "GetTaxiStopsList";
+            cmd.Connection = conn;
+            DataSet ds = new DataSet();
+            SqlDataAdapter db = new SqlDataAdapter(cmd);
+            db.Fill(ds);
+            Tbl = ds.Tables[0];
+
+            // int found = 0;
+            return Tbl;
+        }
+        
+    }
+}
