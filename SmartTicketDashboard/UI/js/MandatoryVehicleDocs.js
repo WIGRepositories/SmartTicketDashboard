4ba9545c2c0c61eb2925058617025fd5b09dfd19
srujansimha <msrujansimha@gmail.com>
2017-10-07 18:52:01 +0530

    vehicle documents

--- SmartTicketDashboard/UI/js/MandatoryVehicleDocs.js
+++ SmartTicketDashboard/UI/js/MandatoryVehicleDocs.js
@@ -9,6 +9,18 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage, $fil
 
     $scope.dashboardDS = $localStorage.dashboardDS;
 
+    $scope.GetAssignedDocuments = function () {
+
+        var ctryId = ($scope.ctry == null || $scope.ctry.Id == null) ? -1 : $scope.ctry.Id;
+        var vgroup = ($scope.vgroup == null || $scope.vgroup.Id == null) ? -1 : $scope.vgroup.Id;
+        $http.get('/api/GetMandatoryVehicleDocs?ctryId=' + ctryId + '&vgId=' + vgroup).then(function (res, data) {
+            $scope.doctypes = res.data;
+            $scope.checkedArr = $filter('filter')($scope.doctypes, { selected: "1" });
+            $scope.uncheckedArr = $filter('filter')($scope.doctypes, { selected: "0" });
+
+        });
+
+    }
     $scope.GetConfigData = function () {
 
         var vc = {
@@ -25,24 +37,13 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage, $fil
 
         $http(req).then(function (res) {
             $scope.initdata = res.data;
-           // $scope.checkedArr = $filter('filter')($scope.VehicleDocs, { Active: "1" });
-            // $scope.uncheckedArr = $filter('filter')($scope.VehicleDocs, { Active: "0" });
+            $scope.ctry = ($scope.initdata.Table1 != null) ? $scope.initdata.Table1[0] : '';
+            $scope.vgroup = ($scope.initdata.Table != null) ? $scope.initdata.Table[0] : '';
             $scope.GetAssignedDocuments();
         });
     }
 
-    $scope.GetAssignedDocuments = function () {
-
-        var ctryId = ($scope.ctry == null || $scope.ctry.Id == null) ? -1 : $scope.ctry.Id;
-        var vgroup = ($scope.vgroup == null || $scope.vgroup.Id == null) ? -1 : $scope.vgroup.Id;
-        $http.get('/api/GetMandatoryVehicleDocs?ctryId=' + ctryId + '&vgId=' + vgroup).then(function (res, data) {
-            $scope.doctypes = res.data;
-            $scope.checkedArr = $filter('filter')($scope.doctypes, { selected: "1" });
-            $scope.uncheckedArr = $filter('filter')($scope.doctypes, { selected: "0" });
-           
-        });
-
-    }
+   
     $scope.checkedArr = new Array();
     $scope.uncheckedArr = new Array();
     
@@ -69,9 +70,26 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage, $fil
                     CountryId: ctry.Id,
                     VehicleGroupId: vgroup.Id,
                     DocTypeId: $scope.checkedArr[cnt].Id,
-                    FileContent: $scope.checkedArr[cnt].FileContent
+                    FileContent: $scope.checkedArr[cnt].FileContent,
+                    IsMandatory: $scope.checkedArr[cnt].IsMandatory,
+                  
                 }
                 VehicleDocs.push(fr);
+            }
+            else {
+                if ($scope.checkedArr[cnt].IsChanged) {
+                    var fr = {
+                        flag: 'U',
+                        Id: -1,
+                        CountryId: ctry.Id,
+                        VehicleGroupId: vgroup.Id,
+                        DocTypeId: $scope.checkedArr[cnt].Id,
+                        FileContent: $scope.checkedArr[cnt].FileContent,
+                        IsMandatory: $scope.checkedArr[cnt].IsMandatory,
+                        
+                    }
+                    VehicleDocs.push(fr);
+                }
             }
         }
 
@@ -83,7 +101,9 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage, $fil
                     CountryId: ctry.Id,
                     VehicleGroupId: vgroup.Id,
                     DocTypeId: $scope.checkedArr[cnt].Id,
-                    FileContent: $scope.checkedArr[cnt].FileContent
+                    FileContent: $scope.checkedArr[cnt].FileContent,
+                    IsMandatory: $scope.checkedArr[cnt].IsMandatory,
+                  
                 }
                 VehicleDocs.push(fr);
             }
@@ -97,7 +117,7 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage, $fil
 
         }).success(function (data, status, headers, config) {
             alert('Documenents details Saved Successfully');
-
+            $scope.GetAssignedDocuments();
         }).error(function (ata, status, headers, config) {
             alert('Documenents details Not Saved ');
         });
@@ -121,6 +141,26 @@ var ctrl = app.controller('myCtrl', function ($scope, $http, $localStorage, $fil
         }
     };
 
+    $scope.toggleMandatory = function (item) {
+        var idx = $scope.checkedArr.indexOf(item);
+        if (idx > -1) {
+            //$scope.checkedArr.splice(idx, 1);
+            $scope.checkedArr[idx].IsMandatory = ($scope.checkedArr[idx].IsMandatory == 0) ? 1 : 0;
+            $scope.checkedArr[idx].IsChanged = 1;
+        }
+        else {
+            item.IsMandatory = 1;
+            item.IsChanged = 1;
+            $scope.checkedArr.push(item);
+        }
+    };
+    $scope.existsMandatory = function (item, list) {
+        if (list == null) return false;
+        if (list.indexOf(item) > -1 && item.IsMandatory == 1)
+            return true;
+        else
+            false;
+    };
     $scope.exists = function (item, list) {
         if (list == null) return false;
         return list.indexOf(item) > -1;
